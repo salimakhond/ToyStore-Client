fe30aee60f58a78a1edd23f9ab90e0477b85407d
salimakhond <salimakhond@gmai.com>
2023-05-18 02:10:11 +0600

    register page desgine added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,51 @@
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
     return (
-        <div>
-            register page
+        <div className="py-[50px] md:py-[80px] lg:py-[130px] bg-white">
+            <div className="">
+                <div className="card m-auto w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h2 className="text-5xl font-bold text-center">Register</h2>
+                        <form >
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo</span>
+                                </label>
+                                <input type="text" name='photo' placeholder="Photo URL" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary" type="submit" value="Sign In" />
+                            </div>
+                        </form>
+                        <p className='my-5 text-center'>All Ready Have An Account ?
+                            <Link className='text-orange-500 font-bold ml-3' to='/login'>Login</Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
