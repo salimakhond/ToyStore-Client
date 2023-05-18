52fb9a8a9ef3359aa36a5633817fa03a3285fd29
salimakhond <salimakhond@gmai.com>
2023-05-19 03:35:28 +0600

    category by active btn

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -5,17 +5,21 @@ import ShopCategory from "../ShopCategory/ShopCategory";
 
 const Home = () => {
     const [products, setProducts] = useState([]);
+    const [activeBtn, setActiveBtn] = useState("");
 
 
     useEffect(() => {
-        fetch(`http://localhost:5000/products`)
+        fetch(`http://localhost:5000/products/${activeBtn}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, [])
+    }, [activeBtn])
 
 
 
 
+    const handleTabClick = (tabName) => {
+        setActiveBtn(tabName);
+    };
 
 
     return (
@@ -23,7 +27,30 @@ const Home = () => {
             <Banner></Banner>
             {
                 <div>
-                    
+                    <div className="text-center mb-5">
+                        <div className="btn-group btn-group-vertical lg:btn-group-horizontal ">
+                            <button
+                                style={{ textTransform: 'capitalize', borderRight: '2px solid #fff' }}
+                                onClick={() => handleTabClick("")}
+                                className={`btn American Doll ${activeBtn == "" ? " btn-active" : ""
+                                    }`}>All Doll</button>
+                            <button
+                                style={{ textTransform: 'capitalize', borderRight: '2px solid #fff' }}
+                                onClick={() => handleTabClick("American Doll")}
+                                className={`btn American Doll ${activeBtn == "American Doll" ? " btn-active" : ""
+                                    }`}>American Doll</button>
+                            <button
+                                style={{ textTransform: 'capitalize', borderRight: '2px solid #fff' }}
+                                onClick={() => handleTabClick("Baby Doll")}
+                                className={`btn Baby Doll ${activeBtn == "Baby Doll" ? " btn-active" : ""
+                                    }`}>Baby Doll</button>
+                            <button
+                                style={{ textTransform: 'capitalize' }}
+                                onClick={() => handleTabClick("Barbie Doll")}
+                                className={`btn Barbie Doll ${activeBtn == "Barbie Doll" ? " btn-active" : ""
+                                    }`}>Barbie Doll</button>
+                        </div>
+                    </div>
 
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         {
